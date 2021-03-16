12c7ef0c935347cc32f2ebee05f26208a7d7aabc
Andrii Kushnir <andkushnir@gmail.com>
2021-03-16 15:07:41 +0200

    First commit ver 1.0

---
+++ js/script.js
@@ -0,0 +1,15 @@
+ let number = 5; 
+ const leftBorderWidth = 1;
+
+// alert ('hello world');
+//const result = confirm ('Are you here?');
+//alert (result)
+
+//const answer = prompt ('Вам есть 18?', 'yes yes yes');
+//console.log(+answer + 10);
+
+const category = 'toys';
+const myURL = 'https://someurl.com/' + category;
+const myURL2 = `https://someurl.com/${category}/5/${myURL}`;
+alert (myURL);
+alert (myURL2);
